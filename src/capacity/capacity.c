d546ca44df816b2caf247072e72fee1fa443aebb
Konnor <konnor.frik666@gmail.com>
2024-06-29 17:25:23 +0500

    write func for capacity management

---
+++ src/capacity/capacity.c
@@ -0,0 +1,64 @@
+#include "../../cvector.h"
+#include <stdlib.h>
+
+bool cvector_empty(cvector* obj) {
+    if ( !obj ) {
+        return true;
+    }
+
+    return obj->index == 0;
+}
+
+size_t cvector_size(cvector* obj) {
+    if ( !obj ) {
+        return 0;
+    }
+
+    return obj->index;
+}
+
+bool cvector_reserve(cvector* obj, size_t new_size) {
+    if ( !obj || new_size <= obj->capacity ) {
+        return true;
+    }
+
+    bool status = false;
+    void** tmp = realloc(obj->array, new_size * sizeof(void*));
+
+    if ( tmp ) {
+        obj->array = tmp;
+        obj->capacity = new_size;
+
+    } else {
+        status = true;
+    }
+
+    return status;
+}
+
+size_t cvector_capacity(cvector* obj) {
+    if ( !obj ) {
+        return 0;
+    }
+
+    return obj->capacity;
+}
+
+bool cvector_shrink_to_fit(cvector* obj) {
+    if ( !obj ) {
+        return true;
+    }
+
+    bool status = false;
+    void** tmp = realloc(obj->array, obj->index * sizeof(void*));
+
+    if ( tmp ) {
+        obj->array = tmp;
+        obj->capacity = obj->index;
+
+    } else {
+        status = true;
+    }
+
+    return status;
+}
