e0b10a8b3ee6fd92dd0f123fcca60df96908f11f
Konnor <konnor.frik666@gmail.com>
2024-06-29 17:27:51 +0500

    work with todos

--- cvector.h
+++ cvector.h
@@ -14,16 +14,15 @@
 
 #include <stddef.h>
 #include <stdbool.h>
+// TODO: target for makefile: static and shared libs
 
 /*
  * Rules:
- * - Is struct open for user ?
+ * - Is struct open for user ? Yes
  * - User can access only @Main functions
  * - FuncDesign:
- *      - What func must return (Status, Data, Mixed) ? Mixed
- *          - If func return status, what type is it (int, uint8_t, typedef'ed enum) ? bool
- *          - If func return status, this is true,false or more specific status-code ? true-false
- *      - What to do if can't allocate memory ? return false as error
+ *      - Some func return common status-code (true, false), some result of it work
+ *      - In every unexpected situation 'false' will returns. It may be real error (can't allocate memory), or wrong argument passed, or anything else
  *      - All func have prefix cvector_*
  */
 
@@ -37,12 +36,6 @@ struct cvector_ {
 };
 
 typedef struct cvector_ cvector;
-
-// TODO: [X] Create & delete
-// [IT] new - arg: size; return pointer to a new cvector object; call ctor     @Main
-// [IT] ctor - arg: size, cvector; allow size = 0; init cvector struct inside           @Add-on
-// [IT] dtor - delete cvector ONLY inside                                      @Add-on
-// [IT] delete - arg: pointer to cvector; Call dtor, safe free pointer         @Main
 
 /** @brief Allocate and create new cvector object
  * @param[in] size Initial size of cvector storage
@@ -74,15 +67,8 @@ void cvector_dtor(cvector* obj);
  * @ingroup Main
  */
 void cvector_delete(cvector* obj);
-// TODO: cvector_delete return new state of deleted object - always NULL
-// variable can be fast overrided with new state
-
-
-// TODO: [ ] element access
-// [ ] at - arg: position; return element at specified position
-// [ ] front - return first element
-// [ ] back - return last element
-// [ ] data - return pointer to raw array with data
+// TODO: [ ] cvector_delete return new state of deleted object - always NULL
+// variable can be fast overrided with new state! :)
 
 /** @brief access specified element with bounds checking
  * @param[in] obj cvector object
@@ -116,14 +102,6 @@ void* cvector_back(cvector* obj);
  * @ingroup Main
  */
 void* cvector_data(cvector* obj);
-
-
-// TODO: [ ] capacity
-// [ ] empty - is object empty
-// [ ] size - return size of writed objects
-// [ ] reserve - arg: size; allocate more memory
-// [ ] capacity - returns the number of elements that can be held in currently allocated storage
-// [ ] shrink_to_fit - reduces memory usage by freeing unused memory
 
 /** @brief Status is storage empty
  * @param[in] obj cvector object
@@ -166,15 +144,6 @@ size_t cvector_capacity(cvector* obj);
  */
 bool cvector_shrink_to_fit(cvector* obj);
 
-
-// TODO: [ ] modifiers
-// [I ] clear - clears the contents
-// [I ] insert - arg: position; inserts elements into concrete position
-// [I ] erase - arg: position; erases element at pos
-// [I ] push_back - arg: pointer to any object; append new pointer at the end
-// [I ] pop_back - delete pointer from end
-// [I ] swap - arg: cvector; swaps the contents
-
 /** @brief Clear all storage
  * @param[in] obj cvector object
  * @return status false:OK true:Error
@@ -182,7 +151,6 @@ bool cvector_shrink_to_fit(cvector* obj);
  * @ingroup Main
  */
 bool cvector_clear(cvector* obj);
-
 
 /** @brief Insert given element at given position
  * @param[in, out] obj cvector object
@@ -228,10 +196,6 @@ bool cvector_pop_back(cvector* obj);
  * @ingroup Main
  */
 bool cvector_swap(cvector* obj_1, cvector* obj_2);
-
-
-// TODO: [ ] Add-ons
-// [I ] increase_memory - arg: cvector; increase memory with formula capacity = capacity * 2
 
 /** @brief Increase inner memory and reallocate storage
  * @param[in, out] obj cvector object
