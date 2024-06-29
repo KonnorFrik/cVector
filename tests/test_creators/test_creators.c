c16b1bb16f4c35020a9dcf18d78b1cb0f5256f21
Konnor <konnor.frik666@gmail.com>
2024-06-29 14:42:50 +0500

    fix naming

--- tests/test_creators/test_creators.c
+++ tests/test_creators/test_creators.c
@@ -7,7 +7,7 @@ void test_creators_create_zero_size(void** state) {
     assert_non_null(obj);
     assert_non_null(obj->array);
     assert_int_equal(obj->index, 0);
-    assert_int_equal(obj->capasity, size);
+    assert_int_equal(obj->capacity, size);
 
     cvector_delete(obj);
 }
@@ -19,7 +19,7 @@ void test_creators_create_n_size(void** state) {
     assert_non_null(obj);
     assert_non_null(obj->array);
     assert_int_equal(obj->index, 0);
-    assert_int_equal(obj->capasity, size);
+    assert_int_equal(obj->capacity, size);
 
     cvector_delete(obj);
 }
@@ -32,12 +32,21 @@ void test_creators_dtor_1(void** state) {
 
     assert_null(obj->array);
     assert_int_equal(obj->index, 0);
-    assert_int_equal(obj->capasity, 0);
+    assert_int_equal(obj->capacity, 0);
 
     cvector_delete(obj);
 }
 
 void test_creators_ctor_bad_1(void** state) {
     assert_true(cvector_ctor(NULL, 0));
+}
+
+void test_creators_ctor_1(void** state) {
+    size_t size = 3;
+    cvector obj = {0};
+
+    assert_false(cvector_ctor(&obj, size));
+    assert_non_null(obj.array);
+    assert_int_equal(obj.capacity, size);
 }
 
